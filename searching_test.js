fe45a485dda951a15ac35bf8037f577bb01cfd23
dilarakanmaz <47085824+dilarakanmaz@users.noreply.github.com>
2020-02-20 10:10:34 +0300

    Delete searching_test.js

--- searching_test.js
+++
@@ -1,9 +0,0 @@
-Feature('Searching');
-
-Scenario('Search an item', (I) => {
-    I.amOnPage('/');
-    I.fillField('Search','blouse');
-    I.click('Search');
-    I.see('1 result has been found.');
-    
-});
